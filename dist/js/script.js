e7f97c0ae66c99efb0cb2850dd63f9c089b811ab
Fauzul Akbar <fauzulakbar2575@gmail.com>
2025-01-04 20:53:27 +0700

    feat(navbar): make navbar responsive

---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".ri-menu-3-line");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", ()=>{
+    menu.classList.toggle("menu-active");
+});
+
+window.onscroll = () => {
+    menu.classList.remove("menu-active");
+}; 
