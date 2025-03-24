d57f18dd97cfd67a245bbec0e8bdefc7c5254e82
Fauzul Akbar <fauzulakbar2575@gmail.com>
2025-03-24 12:20:05 +0700

    add fqa and complete contact page

---
+++ dist/assets/js/faq.js
@@ -0,0 +1,19 @@
+// FAQ
+document.addEventListener('DOMContentLoaded', function() {
+    const faqQuestions = document.querySelectorAll('.faq-question');
+    
+    faqQuestions.forEach(question => {
+        question.addEventListener('click', function() {
+            this.classList.toggle('active');
+            const answer = this.nextElementSibling;
+            answer.classList.toggle('active');
+            
+            faqQuestions.forEach(otherQuestion => {
+                if (otherQuestion !== this) {
+                    otherQuestion.classList.remove('active');
+                    otherQuestion.nextElementSibling.classList.remove('active');
+                }
+            });
+        });
+    });
+});
--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -9,30 +9,3 @@ hamburger.addEventListener("click", ()=>{
 window.onscroll = () => {
     menu.classList.remove("menu-active");
 }; 
-
-// Overlay Youtube Video
-document.querySelectorAll(".video-container").forEach(container => {
-    container.addEventListener("click", function() {
-        let videoId = this.getAttribute("data-video");
-        let videoFrame = document.getElementById("video-frame");
-        let overlay = document.querySelector(".video-overlay");
-
-        videoFrame.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
-        overlay.style.display = "flex"; // Tampilkan overlay
-    });
-});
-
-document.querySelector(".close-btn").addEventListener("click", function() {
-    let overlay = document.querySelector(".video-overlay");
-    let videoFrame = document.getElementById("video-frame");
-
-    overlay.style.display = "none";
-    videoFrame.src = ""; // Hentikan video
-});
-
-document.querySelector(".video-overlay").addEventListener("click", function(e) {
-    if (e.target === this) {
-        this.style.display = "none";
-        document.getElementById("video-frame").src = ""; // Hentikan video
-    }
-});
